8ce0912194e7b891ede6b34d32764c6e5c16afc8
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-03 09:39:47 +0200

    Initial Commit

---
+++ CustomShapes/CustomShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesApp.swift
+//  CustomShapes
+//
+//  Created by Skorobogatow, Christian on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
